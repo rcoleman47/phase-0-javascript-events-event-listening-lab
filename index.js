1b305785d2afef881cab233a82eb95a9d7d6c088
ryanadams <rcoleman.adams@gmail.com>
2022-04-10 16:19:14 -0500

    commit tree e5db88d49859779e7bd26a944648d482cf595ef1

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById(`input`)
 function addingEventListener() {
+    const click = input.addEventListener('click', ()=> (`I was clicked`))
+    return click
 }
+
+console.log(addingEventListener())
+
